d5dffca2b890cbf4402ad19aff9b868b843ab9c9
Randark <koldaev.alexey@gmail.com>
2022-07-21 23:53:48 +0300

    добавил взаимодействие с localstorage

--- main.js
+++ main.js
@@ -3,3 +3,24 @@ const addTaskBtn = document.querySelector(".add-btn"),
   taskDescriptionInput = document.querySelector(".todos-description"),
   deleteTodos = document.querySelector(".btn-delete"),
   chbox = document.getElementsByClassName(".btn-complete");
+
+const tasks = ["поесть"];
+
+function addTask() {
+  tasks.forEach((task, key) => {
+    localStorage.setItem(key, task);
+  });
+}
+
+function updateTasks() {
+  //todo
+}
+
+function deleteTask() {
+  //todo
+}
+
+addTaskBtn.addEventListener("click", () => {
+  tasks.push(taskDescriptionInput.value);
+  addTask(tasks);
+});
