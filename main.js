7538eff067158fd01d3d452a7cbe7693dc78f1e9
Randark <koldaev.alexey@gmail.com>
2022-07-21 23:35:26 +0300

    Переделываю todo, сделаю через localstorage

--- main.js
+++ main.js
@@ -1,30 +1,5 @@
-const addTaskBtn = document.querySelector(".add-btn");
-const todosWrapper = document.querySelector(".todos-wrapper");
-const taskDescriptionInput = document.querySelector(".todos-description");
-const deleteTodos = document.querySelector(".btn-delete");
-
-var idTodos = 0;
-
-const addTask = () => {
-  return `
-    <div class="todo-item" onclick="completeTask(event)" id="${idTodos}">
-        <div class="description">${taskDescriptionInput.value}</div>
-        <div class="buttons">
-            <input  "class="btn-complete" type="checkbox">
-            <button class="btn-delete"  onclick="deleteTask(event) "id="${idTodos}" >Delete</button>
-        </div>
-    </div>
-    `;
-};
-
-const newElement = () => {
-  taskDescriptionInput.value == ""
-    ? alert("Поле не должно быть пустым!")
-    : (todosWrapper.innerHTML += addTask(idTodos));
-  taskDescriptionInput.value = "";
-  idTodos++;
-};
-
-const deleteTask = (event) => {
-  document.getElementById(event.target.id).remove();
-};
+const addTaskBtn = document.querySelector(".add-btn"),
+  todosWrapper = document.querySelector(".todos-wrapper"),
+  taskDescriptionInput = document.querySelector(".todos-description"),
+  deleteTodos = document.querySelector(".btn-delete"),
+  chbox = document.getElementsByClassName(".btn-complete");
